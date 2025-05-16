96df3453aa4a4767d37ca742f41c4c32dc02e881
kougen <kougen@pixiv.co.jp>
2025-05-16 11:43:46 +0900

    GitHubのリンク更新

--- src/components/githubLink.tsx
+++ src/components/githubLink.tsx
@@ -3,13 +3,13 @@ export const GitHubLink = () => {
     <div className="absolute right-0 z-10 m-24">
       <a
         draggable={false}
-        href="https://github.com/pixiv/ChatVRM"
+        href="https://github.com/pixiv/local-chat-vrm"
         rel="noopener noreferrer"
         target="_blank"
       >
         <div className="p-8 rounded-16 bg-[#1F2328] hover:bg-[#33383E] active:bg-[565A60] flex">
           <img
-            alt="https://github.com/pixiv/ChatVRM"
+            alt="https://github.com/pixiv/local-chat-vrm"
             height={24}
             width={24}
             src={"./github-mark-white.svg"}
